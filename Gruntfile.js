4a9ea67b6f26ffaf2dc2418a3c7fa9e34e6a7c2d
atscott <atscott@google.com>
2018-07-25 13:58:02 -0700

    remove old sw exec

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,12 @@ module.exports = (grunt) => {
     pkg: grunt.file.readJSON('package.json'),
     exec: {
       generate_fortunes: 'node --use-strict src/util/generate_fortunes_list.js',
-      build: {command: 'ng build --prod', options: {maxBuffer: 1024*1000}}
+      build: {command: 'ng build --prod', options: {maxBuffer: 1024 * 1000}}
     }
   });
 
   grunt.loadNpmTasks('grunt-exec');
 
-  grunt.registerTask('build', ['exec:generate_fortunes', 'exec:build', 'exec:sw']);
+  grunt.registerTask('build', ['exec:generate_fortunes', 'exec:build']);
   grunt.registerTask('gen_fortunes', ['exec:generate_fortunes']);
 };
